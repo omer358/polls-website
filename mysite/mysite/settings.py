3d20829732f810be3a3a78809aa02555c53cbb8f
omer358 <omermaki358@gmail.com>
2020-10-05 15:02:56 +0200

    part5

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'GMT'
+TIME_ZONE = 'Africa/Khartoum'
 
 USE_I18N = True
 
--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from django.contrib import admin
+
 from .models import Question, Choice
-
-
-admin.site.register(Question)
-admin.site.register(Choice)
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -12,4 +8,8 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('question', 'choice_text', ' votes')
+    list_display = ('question', 'choice_text', 'votes')
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
