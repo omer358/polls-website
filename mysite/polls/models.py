3b3af98176c17f60c5ed95d6fccbe27e465bff46
omer358 <omermaki358@gmail.com>
2021-02-14 19:43:47 +0200

    part7

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,6 +11,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
